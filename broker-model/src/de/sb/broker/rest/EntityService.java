3e4583cf462ceee38f1d7ce807214465cdf52287
Thomas Engelhard <engelhard90@gmail.com>
2016-11-01 21:28:18 +0100

    moved test

--- broker-model/src/de/sb/broker/rest/EntityService.java
+++ broker-model/src/de/sb/broker/rest/EntityService.java
@@ -27,13 +27,6 @@ import de.sb.java.TypeMetadata;
 @TypeMetadata(copyright = "2013-2015 Sascha Baumeister, all rights reserved", version = "1.0.0", authors = "Sascha Baumeister")
 public class EntityService {
 
-	public static void main(String[] args) {
-		final EntityManager entityManager = Persistence.createEntityManagerFactory("broker").createEntityManager();
-		entityManager.getTransaction().begin();
-		Query query = entityManager.createQuery("select p from Person p");
-		System.out.println(query.getResultList());
-		entityManager.getTransaction().commit();
-	}
 	/**
 	 * Deletes the entity matching the given identity, or does nothing if no such entity exists.
 	 * @param authentication the HTTP Basic "Authorization" header value
